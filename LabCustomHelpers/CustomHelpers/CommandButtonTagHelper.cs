379567c7e80a5e3e6533aba8844f04d7c106daa2
Narendran2020 <72653314+Narendran2020@users.noreply.github.com>
2023-02-14 13:33:04 +0530

    InitialCommit

---
+++ LabCustomHelpers/CustomHelpers/CommandButtonTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LabCustomHelpers.CustomHelpers
+{
+    [HtmlTargetElement("command-button")]
+    public class CommandButtonTagHelper : TagHelper
+    {
+        [HtmlAttributeName("value")]
+        public string Value { get; set; }
+
+        [HtmlAttributeName("class")]
+        public string CssClass { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string strHtml = $"<button type='submit' name='command' ass='{CssClass}'>{Value}</button>";
+            output.Content.SetHtmlContent(strHtml);
+        }
+    }
+}
---
+++ LabCustomHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabCustomHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
